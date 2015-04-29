7ea87c1a9bb6938c3b65d3f5dda840969ce1a7b8
Patrick Allen <prallen90@gmail.com>
2015-04-29 15:25:49 -0400

    Lesson 4 todo 5

--- conference.py
+++ conference.py
@@ -50,6 +50,22 @@ DEFAULTS = {
     "topics" : ["Default", "Topic"],
 }
 
+OPERATORS = {
+            'EQ':   '=',
+            'GT':   '>',
+            'GTEQ': '>=',
+            'LT':   '<',
+            'LTEQ': '<=',
+            'NE':   '!='
+            }
+
+FIELDS =    {
+            'CITY': 'city',
+            'TOPIC': 'topics',
+            'MONTH': 'month',
+            'MAX_ATTENDEES': 'maxAttendees',
+            }
+
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 @endpoints.api( name='conference',
@@ -128,14 +144,60 @@ class ConferenceApi(remote.Service):
         return request
 
 
+    def _getQuery(self, request):
+        """Return formatted query from the submitted filters."""
+        q = Conference.query()
+        inequality_filter, filters = self._formatFilters(request.filters)
+
+        # If exists, sort on inequality filter first
+        if not inequality_filter:
+            q = q.order(Conference.name)
+        else:
+            q = q.order(ndb.GenericProperty(inequality_filter))
+            q = q.order(Conference.name)
+
+        for filtr in filters:
+            if filtr["field"] in ["month", "maxAttendees"]:
+                filtr["value"] = int(filtr["value"])
+                formatted_query = ndb.query.FilterNode(filtr["field"], filtr["operator"], filtr["value"])
+                q = q.filter(formatted_query)
+        return q
+
+
+    def _formatFilters(self, filters):
+        """ Parse, check validity and format user supplied filters."""
+        formatted_filters = []
+        inequality_field = None
+
+        for f in filters:
+            filtr = {field.name: getattr(f, field.name) for field in f.all_fields()}
+
+            try:
+                filtr["field"] = FIELDS[filtr["field"]]
+                filtr["operator"] = OPERATORS[filtr["operator"]]
+            except KeyError:
+                raise endpoints.BadRequestException("Filter contains invalid filed or operator.")
+
+            if filtr["operator"] != "=":
+                # check if inequality operation has been used in previous filters
+                # disallow the filter if inequality was performed on a different field before
+                # track the field on which the inequality operation is performed
+                if inequality_field and inequality_field != filtr["field"]:
+                    raise endpoints.BadRequestException("Inequality filter is allowed on only one field.")
+                else:
+                    inequality_field = filtr["field"]
+
+            formatted_filters.append(filtr)
+        return (inequality_field, formatted_filters)
+
+
     @endpoints.method(ConferenceQueryForms, ConferenceForms,
                       path='queryConferences',
                       http_method='POST',
                       name='queryConferences')
     def queryConferences(self, request):
         """Query for conferences."""
-        conferences = Conference.query()
-
+        conferences = self._getQuery(request)
         return ConferenceForms(
             items=[self._copyConferenceToForm(conf, "") for conf in conferences]
         )
@@ -162,7 +224,7 @@ class ConferenceApi(remote.Service):
     def filterPlayground(self, request):
         """Playing around with filters"""
 
-        q = Conference.query().filter(Conference.city == 'Tokyo').filter(Conference.seatsAvailable < 10).filter(Conference.seatsAvailable > 0).order(
+        q = Conference.query().filter(Conference.city == 'Tokyo').filter(Conference.seatsAvailable < 10).filter(Conference.seatsAvailable > 0)
 
         
         return ConferenceForms(
@@ -241,7 +303,6 @@ class ConferenceApi(remote.Service):
     def saveProfile(self, request):
         """Update & return user profile."""
         return self._doProfile(request)
-
 
 # registers API
 api = endpoints.api_server([ConferenceApi]) 
